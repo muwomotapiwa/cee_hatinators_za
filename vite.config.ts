625f4481c6f725fd592224395d04a8daf11ec427
mathew_muwomo <116170969+muwomotapiwa@users.noreply.github.com>
2025-07-07 23:22:55 +0200

    Deploy React App to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cee_hatinators_za/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
